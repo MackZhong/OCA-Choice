e8df61d676fccbe75cf7669ef85b0026d10276e6
mackzxh <mackz@sina.com>
2016-01-04 10:03:18 +0800

    modify oca_add_repo.sh

--- oca_add_repo.sh
+++ oca_add_repo.sh
@@ -2,16 +2,16 @@
 
 #set -o errexit
 
-VERION="8.0"
+VERSION="8.0"
 if [ -n "${2}" ]; then
-    VERION="${2}"
+    VERSION="${2}"
 fi
 
 if [ -n "${1}" ]; then
     git remote add -f ${1} git@github.com:OCA/${1}.git
-    git subtree add pull --prefix=${1} ${1} $VERION --squash
+    git subtree add pull --prefix=${1} ${1} $VERSION --squash
     rm -rf ~/.local/share/Odoo/addons/$VERSION/${1}
-    ln ~/.local/share/Odoo/addons/$VERSION/${1} ${1}
+    find ${1} -maxdepth 1 -mindepth 1 -type d | xargs ln -st ~/.local/share/Odoo/addons/$VERSION/
 else
     echo "${0} repo-name version"
 fi
