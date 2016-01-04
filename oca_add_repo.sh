8b2b7416f599e57d2c1ce161bda103c188f2ca3a
mackzxh <mackz@sina.com>
2016-01-04 10:06:10 +0800

    x

--- oca_add_repo.sh
+++ oca_add_repo.sh
@@ -9,7 +9,7 @@ fi
 
 if [ -n "${1}" ]; then
     git remote add -f ${1} git@github.com:OCA/${1}.git
-    git subtree add pull --prefix=${1} ${1} $VERSION --squash
+    git subtree add --prefix=${1} ${1} $VERSION --squash
     rm -rf ~/.local/share/Odoo/addons/$VERSION/${1}
     find ${1} -maxdepth 1 -mindepth 1 -type d | xargs ln -st ~/.local/share/Odoo/addons/$VERSION/
 else
